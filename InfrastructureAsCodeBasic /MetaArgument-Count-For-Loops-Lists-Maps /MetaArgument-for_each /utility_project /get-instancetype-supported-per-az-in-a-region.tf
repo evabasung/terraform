cda6108cff476081ef13b7e71e573e664969ca95
evabasung <evabasung@outlook.com>
2025-05-02 17:42:12 -0400

    Update get-instancetype-supported-per-az-in-a-region.tf

--- InfrastructureAsCodeBasic /MetaArgument-Count-For-Loops-Lists-Maps /MetaArgument-for_each /utility_project /get-instancetype-supported-per-az-in-a-region.tf
+++ InfrastructureAsCodeBasic /MetaArgument-Count-For-Loops-Lists-Maps /MetaArgument-for_each /utility_project /get-instancetype-supported-per-az-in-a-region.tf
@@ -1,3 +1,4 @@
+# This code also to check if t3micro is available in said az same as below code
 # Datasource
 
 provider "aws" {
@@ -21,4 +22,27 @@ data "aws_ec2_instance_type_offerings" "my_ins_type1" {
 # Output
 output "output_v1_1" {
   value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types      #instance_types is the attr of aws_ec2_instance_type_offerings
+}
+
+#********************************************************************8
+# This code also to check if t3micro is available in said az same as above code
+
+# Datasource
+data "aws_ec2_instance_type_offerings" "my_ins_type1" {
+  filter {
+    name   = "instance-type"
+    values = ["t3.micro"]
+  }
+  filter {
+    name   = "location"
+    #values = ["us-east-1a"]
+    values = ["us-east-1e"]    
+  }
+  location_type = "availability-zone"
+}
+
+
+# Output
+output "instances-supported" {
+ value = data.aws_ec2_instance_type_offerings.my_ins_type1.instance_types
 }
