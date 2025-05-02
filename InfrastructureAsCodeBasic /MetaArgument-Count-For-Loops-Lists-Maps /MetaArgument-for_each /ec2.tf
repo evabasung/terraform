b1f7000f1b72875a599085e6bbac5bf87e2afe35
evabasung <evabasung@outlook.com>
2025-05-02 15:42:09 -0400

    Create ec2.tf

---
+++ InfrastructureAsCodeBasic /MetaArgument-Count-For-Loops-Lists-Maps /MetaArgument-for_each /ec2.tf
@@ -0,0 +1,17 @@
+
+# EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  user_data = file("${path.module}/app1-install.sh")
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  # Create EC2 Instance in all Availabilty Zones of a VPC  
+  for_each = toset(data.aws_availability_zones.my_azones.names)
+  availability_zone = each.key # You can also use each.value because for list items each.key == each.value
+  tags = {
+    "Name" = "For-Each-Demo-${each.key}" #each az name will be affix on instance so For-Each-Demoxxxxinstancename
+  }
+}
+# This execution will fail since t3micro is not in us-east-1e az
+#let us fix in next usti;ity project
